61bf4ff2e0daeba4e9f6432c83a95e7bff2666ab
hallvman <hallvman@stud.ntnu.no>
2021-11-15 18:07:07 +0100

    #2 - I'm not sure if i've done it correctly

--- ticketsystem_torgeir.sol
+++ ticketsystem_torgeir.sol
@@ -4,36 +4,56 @@ pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Ticket is ERC721{
+contract TICKET is ERC721{
     uint256 public tokenCounter;
+    mapping(uint256 => Ticket) public tickets;
+
+    struct Ticket {
+        address owner;
+        uint256 row;
+        uint256 s_number;
+    }
+
     constructor () ERC721("Ticket","TCKT"){
-        tokenCounter += 0;
+        tokenCounter = 0;
     }
-    
-    function buy(address _to) public{
-        tokenCounter = tokenCounter + 1;
-        _safeMint(_to);
+
+    function buy(address _to, uint256 _row, uint256 _seat_number) public returns (uint256){
+        uint256 newTokenId = tokenCounter + 1;
+
+        tickets[newTokenId] = Ticket(_to, _row, _seat_number);
+        _safeMint(_to, newTokenId);
+
+        return newTokenId;
     }
-    
+
 
 }
 contract Poster is ERC721{
+    uint256 public posterTokenCount;
+
     constructor () ERC721("Poster","PSTR"){
-        
+        posterTokenCount = 0;
     }
-    function buy(address _to) public{
-        
+
+    function buy(address _to) public returns (uint256){
+        uint256 newTokenId = posterTokenCount;
+
+        _safeMint(_to, newTokenId);
+
+        return newTokenId;
     }
 }
+
 contract Show {
     string show_title;
     int256 n_rows = 3;
     int256 n_seats_per_row = 20;
     string link = "https://seatplan.com/";
     mapping(bytes32 => Seat) public seats;
-    
-    Ticket myTicket = new Ticket();
-    
+
+    TICKET myTicket = new TICKET();
+
     struct Seat {
         address owner;
         string title;
@@ -43,7 +63,7 @@ contract Show {
         int256 s_number;
         string set_view_link;
     }
-    
+
     constructor (string memory _show_title, string memory date, int256 price){
         show_title = _show_title;
         for (int256 r_nr = 0; r_nr < n_rows; r_nr++) {
@@ -53,65 +73,65 @@ contract Show {
             }
         }
     }
-    
+
     /*
-    
+
     implement a function buy that B and C individually call to get a ticket
     each, which corresponds to a specific show, date and seat. The function generates
     and transfers a unique ticket upon purchase, as an instance of the TICKET token;
-    
+    string memory _show_title, string memory date, uint256 row, uint256 seat_number
     */
-    
-    function buy() public {
-        myTicket.buy(msg.sender);
+
+    function buy(uint256 row, uint256 seat_number) public {
+        myTicket.buy(msg.sender, row, seat_number);
     }
-    
+
     /*
-    
+
     implement a function verify that allows anyone with the token ID to check
     the validity of the ticket and the address it is supposed to be used by;
-    
+
     */
-    
+
     function verify() public {
-        
+
     }
-    
+
     /*
-    
+
     implement a function refund to refund tickets if a show gets cancelled;
-    
+
     */
-    
+
     function refund() public {
-        
+
     }
-    
+
     /*
-    
+
     implement a function validate to validate a ticket; it can be called only
     in a specific time frame, corresponding to a suitable amount of time before the
     beginning of the show. Upon validation, the ticket is destroyed, and a function
     releasePoster releases unique proof of purchase (you may consider merging these
     two functions together). This new item must be a unique instance of a POSTER
     token;
-    
+
     */
-    
+
     function validate() public {
-        
+
     }
-    
+
     /*
-    
+
     implement a function tradeTicket that allows C and D to safely trade (i.e.
     exchange for another or sell for ether) a ticket directly between each other.
-    
+
     */
-    
+
     function tradeTicket() public {
-        
+
     }
-    
-    
+
+
 }
