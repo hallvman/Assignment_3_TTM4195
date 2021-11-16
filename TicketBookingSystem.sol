16bfe593de5f31e0c106e6d09f7c0492da45eeb4
hallvman <hallvman@stud.ntnu.no>
2021-11-16 10:35:26 +0100

    I think i'm done with task 2

--- TicketBookingSystem.sol
+++ TicketBookingSystem.sol
@@ -4,27 +4,44 @@ pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Ticket is ERC721{
+contract TICKET is ERC721{
+    // Variables for tokenID and mapping for the tickets
     uint256 public tokenCounter;
+    mapping(uint256 => Ticket) public tickets;
+    
+    // Creating the struct to save the tickets
+    struct Ticket {
+        address owner;
+        int256 row;
+        int256 s_number;
+    }
+    
+    // Starts the id at 0 since nothing is added
     constructor () ERC721("Ticket","TCKT"){
-        tokenCounter += 0;
+        tokenCounter = 0;
     }
+
+    // Buy function. Which creates a ticket to each seat. Cannot be bought again
+    function buy(address _to, int256 _row, int256 _seat_number) public returns (uint256){
+        uint256 newTokenId = tokenCounter + 1;
     
-    function buy(address _to) public{
-        tokenCounter = tokenCounter + 1;
-        _safeMint(_to);
+        tickets[newTokenId] = Ticket(_to, _row, _seat_number);
+        _safeMint(_to, newTokenId);
+        
+        return newTokenId;
     }
-    
 
 }
-contract Poster is ERC721{
+contract POSTER is ERC721{
+    uint256 public posterTokenCount;
+    
     constructor () ERC721("Poster","PSTR"){
-        
+        posterTokenCount = 0;
     }
-    function buy(address _to) public{
-        
-    }
+    
+    
 }
+
 contract Show {
     string show_title;
     int256 n_rows = 3;
@@ -32,8 +49,10 @@ contract Show {
     string link = "https://seatplan.com/";
     mapping(bytes32 => Seat) public seats;
     
-    Ticket myTicket = new Ticket();
-    
+    // Creates an instance of the TICKET and POSTER
+    TICKET myTicket = new TICKET();
+    POSTER myTicket = new POSTER();
+
     struct Seat {
         address owner;
         string title;
@@ -43,7 +62,7 @@ contract Show {
         int256 s_number;
         string set_view_link;
     }
-    
+
     constructor (string memory _show_title, string memory date, int256 price){
         show_title = _show_title;
         for (int256 r_nr = 0; r_nr < n_rows; r_nr++) {
@@ -53,65 +72,65 @@ contract Show {
             }
         }
     }
-    
+
     /*
-    
+
     implement a function buy that B and C individually call to get a ticket
     each, which corresponds to a specific show, date and seat. The function generates
     and transfers a unique ticket upon purchase, as an instance of the TICKET token;
-    
+    string memory _show_title, string memory date, uint256 row, uint256 seat_number
     */
-    
-    function buy() public {
-        myTicket.buy(msg.sender);
+
+    function buy(int256 row, int256 seat_number) public {
+        require(row <= 3, "Only 3 rows");
+        require(seat_number <= 20, "Only 20 seats per row");
+        myTicket.buy(msg.sender, row, seat_number);
     }
-    
+
     /*
-    
+
     implement a function verify that allows anyone with the token ID to check
     the validity of the ticket and the address it is supposed to be used by;
-    
+
     */
-    
+
     function verify() public {
         
     }
-    
+
     /*
-    
+
     implement a function refund to refund tickets if a show gets cancelled;
-    
+
     */
-    
+
     function refund() public {
-        
+
     }
-    
+
     /*
-    
+
     implement a function validate to validate a ticket; it can be called only
     in a specific time frame, corresponding to a suitable amount of time before the
     beginning of the show. Upon validation, the ticket is destroyed, and a function
     releasePoster releases unique proof of purchase (you may consider merging these
     two functions together). This new item must be a unique instance of a POSTER
     token;
-    
+
     */
-    
+
     function validate() public {
-        
+
     }
-    
+
     /*
-    
+
     implement a function tradeTicket that allows C and D to safely trade (i.e.
     exchange for another or sell for ether) a ticket directly between each other.
-    
+
     */
-    
+
     function tradeTicket() public {
-        
+
     }
-    
-    
 }
