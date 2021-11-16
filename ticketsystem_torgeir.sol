23be5db64cf65820e0a9f6a3df6898957c66abfd
Torgeir Sandnes Laurvik <32878887+torgesla@users.noreply.github.com>
2021-11-16 14:16:22 +0000

    Update ticketsystem_torgeir.sol

--- ticketsystem_torgeir.sol
+++ ticketsystem_torgeir.sol
@@ -1,73 +1,84 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract TICKET is ERC721{
+contract Ticket is ERC721{
     uint256 public tokenCounter;
-    mapping(uint256 => Ticket) public tickets;
-
-    struct Ticket {
-        address owner;
+    mapping(uint256 => TicketInfo) public tickets;
+    
+    struct TicketInfo {
+        //address owner;
+        string title;
+        uint256 date;
         uint256 row;
         uint256 s_number;
+        uint256 price;
+        string link;
     }
-
+    
     constructor () ERC721("Ticket","TCKT"){
         tokenCounter = 0;
     }
 
-    function buy(address _to, uint256 _row, uint256 _seat_number) public returns (uint256){
-        uint256 newTokenId = tokenCounter + 1;
-
-        tickets[newTokenId] = Ticket(_to, _row, _seat_number);
+    function buy(address _to, string memory _title, uint256 _date, uint256 _row, uint256 _seat_number, uint256 _price, string memory _link) public returns(uint256){
+        uint256 newTokenId = tokenCounter;
+    
+        tickets[newTokenId] = TicketInfo(_title, _date, _row, _seat_number, _price, _link);
         _safeMint(_to, newTokenId);
-
+        
+        tokenCounter = tokenCounter + 1;
         return newTokenId;
     }
-
-
+    
+    function refund(uint256 tokenId) public {
+        _burn(tokenId);
+    }
 }
+
 contract Poster is ERC721{
     uint256 public posterTokenCount;
-
+    
     constructor () ERC721("Poster","PSTR"){
         posterTokenCount = 0;
     }
-
-    function buy(address _to) public returns (uint256){
+    
+    function buy(address _to) public{
         uint256 newTokenId = posterTokenCount;
-
+        
         _safeMint(_to, newTokenId);
-
-        return newTokenId;
     }
 }
 
 contract Show {
+    show_status status = show_status.Scheduled;
+    address systems_wallet = address(1);
     string show_title;
-    int256 n_rows = 3;
-    int256 n_seats_per_row = 20;
+    uint256 n_rows = 3;
+    uint256 n_seats_per_row = 20;
     string link = "https://seatplan.com/";
     mapping(bytes32 => Seat) public seats;
-
-    TICKET myTicket = new TICKET();
+    mapping(uint256 => address payable) public tokenOwners;
+    mapping(address => uint256) public payback_amount;
+    uint256 number_of_tickets_sold = 0;
+    
+    Ticket myTicket = new Ticket();
 
     struct Seat {
         address owner;
         string title;
-        string date;
-        int256 price;
-        int256 s_row;
-        int256 s_number;
+        uint256 date;
+        uint256 price;
+        uint256 s_row;
+        uint256 s_number;
         string set_view_link;
     }
+    enum show_status {Scheduled, On, Over, Cancelled}
 
-    constructor (string memory _show_title, string memory date, int256 price){
+    constructor (string memory _show_title, uint256 date, uint256 price){
         show_title = _show_title;
-        for (int256 r_nr = 0; r_nr < n_rows; r_nr++) {
-            for (int256 c_nr = 0; c_nr < n_seats_per_row; c_nr++) {
+        for (uint256 r_nr = 1; r_nr <= n_rows; r_nr++) {
+            for (uint256 c_nr = 1; c_nr <= n_seats_per_row; c_nr++) {
                 bytes32 mapping_key = keccak256(abi.encodePacked(r_nr, c_nr));
                 seats[mapping_key] = Seat(address(0), _show_title, date, price, r_nr, c_nr, link);
             }
@@ -75,63 +86,48 @@ contract Show {
     }
 
     /*
-
     implement a function buy that B and C individually call to get a ticket
     each, which corresponds to a specific show, date and seat. The function generates
     and transfers a unique ticket upon purchase, as an instance of the TICKET token;
-    string memory _show_title, string memory date, uint256 row, uint256 seat_number
     */
 
     function buy(uint256 row, uint256 seat_number) public {
-        myTicket.buy(msg.sender, row, seat_number);
+        require(status == show_status.Scheduled);
+        require(row >= 1 && row <= 3 , "Pick row number between 1 and 3");
+        require(seat_number >= 1 && seat_number <= 20, "Pick seat number between 1 and 20");
+        
+        bytes32 mapping_key = keccak256(abi.encodePacked(row, seat_number));
+        require(seats[mapping_key].owner == address(0), "This seat is already taken!");
+        
+        number_of_tickets_sold = myTicket.buy(msg.sender, seats[mapping_key].title, seats[mapping_key].date, row, seat_number, seats[mapping_key].price, seats[mapping_key].set_view_link);
+        tokenOwners[number_of_tickets_sold] = payable(msg.sender);
+        seats[mapping_key].owner = msg.sender;
     }
-
-    /*
-
-    implement a function verify that allows anyone with the token ID to check
-    the validity of the ticket and the address it is supposed to be used by;
-
-    */
-
-    function verify() public {
-
+    
+    /* implement a function verify that allows anyone with the token ID to checkthe validity of the ticket and the address it is supposed to be used by */
+    function verify(uint256 tokenID) public view{
+        /* Verify that an address owns a token*/
+        require(tokenOwners[tokenID] == msg.sender);
     }
-
-    /*
-
-    implement a function refund to refund tickets if a show gets cancelled;
-
-    */
-
-    function refund() public {
-
+    
+    function refund() public payable{
+        /* Refund funds to ticket owners in caase of cancelled show */
+        /* Accumulate refund amount for each of the addresses that own ticket(s)*/
+        for (uint256 r_nr = 1; r_nr <= n_rows; r_nr++) {
+            for (uint256 c_nr = 1; c_nr <= n_seats_per_row; c_nr++) {
+                bytes32 mapping_key = keccak256(abi.encodePacked(r_nr, c_nr));
+                Seat memory seat = seats[mapping_key];
+                if(seat.owner != address(0)){
+                    payback_amount[seat.owner] = payback_amount[seat.owner] + seat.price;
+                }
+            }
+        }
+        /* Refund the funds address by address*/
+        for (uint256 token_ID = 0;  token_ID <= number_of_tickets_sold; token_ID++) {
+            address payable owner_address = tokenOwners[token_ID];
+            uint256 amount = payback_amount[owner_address];
+            (bool success, ) = owner_address.call{value:amount}("");
+            require(success, "Transfer failed.");
+        }
     }
-
-    /*
-
-    implement a function validate to validate a ticket; it can be called only
-    in a specific time frame, corresponding to a suitable amount of time before the
-    beginning of the show. Upon validation, the ticket is destroyed, and a function
-    releasePoster releases unique proof of purchase (you may consider merging these
-    two functions together). This new item must be a unique instance of a POSTER
-    token;
-
-    */
-
-    function validate() public {
-
-    }
-
-    /*
-
-    implement a function tradeTicket that allows C and D to safely trade (i.e.
-    exchange for another or sell for ether) a ticket directly between each other.
-
-    */
-
-    function tradeTicket() public {
-
-    }
-
-
 }
